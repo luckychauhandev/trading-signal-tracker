55d10d6591572806b6c9bbbfbaeb863eadac69b7
luckychauhandev <luckychauhan.12w@gmail.com>
2026-05-30 21:55:02 +0530

    feat: apply cors using env variable

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,14 @@
 import express from "express";
+import cors from "cors";
 import signalRoutes from "./routes/signalRoutes.js";
 
 const app = express();
 
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+    })
+);
 app.use(express.json());
 
 app.use("/api/signals", signalRoutes);
